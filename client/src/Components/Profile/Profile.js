f085c7e1f9d974c2c3fba6d6195375ec40c2cb24
OwenGranot <granotowen@gmail.com>
2020-10-06 11:50:38 +0300

    tweets hook

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -8,6 +8,7 @@ import useId from "../../hooks/findId";
 import useProfileInfo from "../../hooks/profileInfo";
 import useFollowers from "../../hooks/followers";
 import useFollowing from "../../hooks/following";
+import useTweets from "../../hooks/tweets";
 
 export default function Profile(props) {
   const name = props.match.params.name;
@@ -15,6 +16,8 @@ export default function Profile(props) {
   const userInfo = useProfileInfo(id);
   const followers = useFollowers(id);
   const following = useFollowing(id);
+  const tweets = useTweets(id);
+  console.log(userInfo);
   const isMyProfile =
     userInfo && parseInt(localStorage.getItem("id")) === userInfo.user_id;
 
@@ -23,7 +26,8 @@ export default function Profile(props) {
       <Nav active={isMyProfile && "profile"} />
       <div className="profile">
         <div className="showcase">
-            
+            <img src={userInfo && userInfo.profile} alt="user_logo" className="logo"/>
+            <h3 className="name">{userInfo && userInfo.username}</h3>
         </div>
       </div>
     </div>
---
+++ client/src/hooks/tweets.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+export default function useTweets(id) {
+  const [tweets, setTweets] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`process.env.REACT_APP_TWEETS/${id}`)
+      .then((response) => setTweets(response))
+      .catch((error) => setTweets(null));
+    return () => {
+      setTweets(null);
+    };
+  }, [id]);
+
+  return tweets;
+}
